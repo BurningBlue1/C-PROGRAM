b2fc5be56b0b6f3f0b3f1f394dd8764c3f4c80a9
BurningBlue1 <1291372044@qq.com>
2023-07-10 22:20:57 +0800

    add WebServer code

--- WebServer/v1.0/main.cpp
+++ WebServer/v1.0/main.cpp
@@ -3,3 +3,33 @@
 #include "thread.h"
 #include "util.h"
 
+#include <sys/epoll.h>
+#include <queue>
+#include <sys/time.h>
+#include <netinet/in.h>
+#inclde <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+const int THREADPOOL_THREAD_NUM = 4;
+const int QUEUE_SIZE = 65535;
+
+const int PORT = 8888;
+const int ASK_STATIC_FILE = 1;
+const int ASK_IMAGE_STITCH = 2;
+
+const string PATH= "/";
+
+const int TIMER_TIME_OUT = 500;
+
+extern pthread_mutx_t qlock;
+extern struct epoll_event* events;
+void acceptConnection(int listen_fd, int epoll_fd, const string &path);
+
+extern priority_queue<mytimer*, deque<mytimer*>, timerCmp> myTimerQueue;
+
+int socket_bind_listen(int port){
+    
+}
+
